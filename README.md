c1ced20474af098a782af818e90a5309f4f98d8b
TictacMenthe <ugurcan.ekren@telecom-sudparis.eu>
2020-11-28 22:19:47 +0100

    readme, run script without bash

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # techtherisc
 Simple RISCV implementation in VHDL
+
+## Dependencies
+- GHDL v0.37+
+- Python3 (works on 3.9.0, doesn't really require much)
+- vunit-hdl v4.4.0+ (from Python pip)
+- GTKWave v3.3.100+ (for GUI wave visualization)
+
+## Running
+HDL Compilation is automatic with VUnit, it automatically detects what entities are needed and compiles them.
+All that is required is that the libraries are created in the root ```run.py``` script, and the corresponding source files are added to them.
+
+To run a testbench named ```test_lib.<the_tb_entity>``` located in a file ```./test_lib/the_tb_entity.vhd```, you can:
+- Run the script ```run.py``` and give it any wildcard based argument that matches the .vhd file of the TB.
+- Run the script ```run.py``` and give it any wildcard based argument that matches the name of the entity (```<the_library>.<the_tb_entity>.<the_test_case>```).
+
+If nothing matches, VUnit automatically runs every testbench found.
+A testbench is a module that includes ```_tb``` or ```tb_``` in its name and the generic parameter ```runner_cfg```.
+
+Some options:
+- If using the first method, giving -g or --gui checks if there are any .gtkw savefiles for GTKWave (easily created using its GUI)
+  If there are no savefiles, just opens GTKWave without any save data
+- Every other VUnit CLI arguments are available (-v for verbose mode, etc).
+
--- run.py
+++ run.py
@@ -1,7 +1,61 @@
+#!python
+import subprocess
+import glob
+import sys
+import os
 from vunit import VUnit
 
-vu = VUnit.from_argv()
+copy_argv = sys.argv[1:]
+
+final_argv = []
+
+waves = []
+
+gui_request = any(x in copy_argv for x in ['-g', '--gui'])
+
+
+# Construct the new argv from input argv by finding the files to use in test (with wildcards pattern matching)
+for i, arg in enumerate(copy_argv):
+  found = glob.glob('test_lib/*'+arg+'*.vhd')
+  if found: # If the pattern matches a .vhd file in test_lib
+     # TB pattern for VUnit, with wildcards to run all test cases for this TB
+    final_argv.append('*'+arg+'*')
+  else: # else, add it raw if not -g
+    if arg not in ['-g', '--gui']:
+      final_argv.append(arg)
+
+  # Records which tests in the list can be opened with gtkwave (already have an .gtkw file)
+  if gui_request:
+    found = glob.glob('test_lib/*'+arg+'*.gtkw')
+    if found:
+      for f in found:
+        waves.append(f)
+
+# Log it, and add -g
+if gui_request:
+  if not waves:
+    final_argv.append('-g')
+    print("Running with GUI without any save file.")
+  else:
+    print("Running with GUI and there are save files.")
+else:
+  print("Running with no GUI.")
+
+
+exit()
+# force gtkwave files to be generated, even if not openned with GUI
+final_argv+=['--gtkwave-fmt', 'ghw']
+
+def show_waves(results):
+  for wave in waves:
+    print(f"Running 'gtkwave {wave}'.")
+    res = subprocess.call(['gtkwave', wave], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    print("GTKWave subprocess returned", res)
+
+vu = VUnit.from_argv(argv=final_argv)
+
 vu.enable_location_preprocessing()
+
 src_lib = vu.add_library("src_lib")
 tool_lib = vu.add_library("tool_lib")
 test_lib = vu.add_library("test_lib")
@@ -13,4 +67,4 @@ src_lib.add_source_files(src_lib.name+"/instr_decoder.vhd")
 test_lib.add_source_files(test_lib.name+"/reg_tb.vhd")
 test_lib.add_source_files(test_lib.name+"/instr_tb.vhd")
 
-vu.main()
+vu.main(show_waves)
